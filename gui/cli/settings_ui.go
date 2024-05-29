1526348ba1dac0d40d84fbba34481b99022c5a0c
Rostyslav <r.kovalenko.it@gmail.com>
2024-05-29 11:53:38 +0200

    Game Settings refactored

---
+++ gui/cli/settings_ui.go
@@ -0,0 +1,187 @@
+package cli
+
+import (
+	"battleship_client/api/client"
+	"context"
+
+	wGui "github.com/RostKoff/warships-gui/v2"
+)
+
+type SettingsUI struct {
+	Controller *wGui.GUI
+	nameInput  *wGui.TextField
+	descInput  *wGui.TextField
+	startBtn   *wGui.Button
+	botBtn     *wGui.Button
+	refreshBtn *wGui.Button
+	BtnArea    *wGui.HandleArea
+	lobbyArea  *wGui.HandleArea
+	lobbyMap   map[string]Row
+	targetNick string
+	targetRow  *Row
+}
+
+// Creates and draws all the elements of the lobby.
+func InitSettings(controller *wGui.GUI) *SettingsUI {
+	// Name Input
+	nameTxt := wGui.NewText(2, 1, "Enter name", nil)
+	nameInCfg := wGui.NewTextFieldConfig()
+	nameInCfg.UnfilledChar = '_'
+	nameInCfg.InputOn = true
+	nameIn := wGui.NewTextField(2, 2, 30, 1, nameInCfg)
+
+	// Description Input
+	descTxt := wGui.NewText(2, 4, "Enter Description", nil)
+	descInCfg := wGui.NewTextFieldConfig()
+	descInCfg.UnfilledChar = '_'
+	descInCfg.InputOn = true
+	descIn := wGui.NewTextField(2, 5, 30, 5, descInCfg)
+
+	// Action Buttons
+	btnCfg := wGui.NewButtonConfig()
+	btnCfg.Width = 0
+	btnCfg.Height = 0
+	btnCfg.Width = 20
+	btnCfg.BgColor = wGui.Green
+	startBtn := wGui.NewButton(2, 11, "Host Game", btnCfg)
+	x, _ := startBtn.Position()
+	w, _ := startBtn.Size()
+	btnCfg.BgColor = wGui.Blue
+	botBtn := wGui.NewButton(x+w+2, 11, "Against Bot", btnCfg)
+	x, _ = botBtn.Position()
+	w, _ = botBtn.Size()
+	btnCfg.BgColor = wGui.Grey
+	refreshBtn := wGui.NewButton(x+w+2, 11, "Refresh", btnCfg)
+
+	// Handle Area for buttons
+	btnMapping := map[string]wGui.Physical{
+		"botBtn":     botBtn,
+		"startBtn":   startBtn,
+		"refreshBtn": refreshBtn,
+	}
+	btnArea := wGui.NewHandleArea(btnMapping)
+
+	// Lobby
+	lCfg := wGui.NewButtonConfig()
+	lCfg.Width = 42
+
+	lobbyTxt := wGui.NewButton(2, 15, "Lobby", lCfg)
+	lCfg.Width = 21
+	nickHead := wGui.NewButton(2, 18, "Nick", lCfg)
+	statusHead := wGui.NewButton(lCfg.Width+2, 18, "Status", lCfg)
+	lobbyArea := wGui.NewHandleArea(nil)
+
+	// Draw all objects
+	drawables := []wGui.Drawable{
+		nameTxt,
+		nameIn,
+		descTxt,
+		descIn,
+		startBtn,
+		botBtn,
+		refreshBtn,
+		btnArea,
+		lobbyTxt,
+		lobbyArea,
+		nickHead,
+		statusHead,
+	}
+	for _, drawable := range drawables {
+		controller.Draw(drawable)
+	}
+
+	return &SettingsUI{
+		Controller: controller,
+		nameInput:  nameIn,
+		descInput:  descIn,
+		startBtn:   startBtn,
+		botBtn:     botBtn,
+		refreshBtn: refreshBtn,
+		BtnArea:    btnArea,
+		lobbyArea:  lobbyArea,
+	}
+}
+
+// Creates a row for each given lobby game and displays them.
+// Also adds them to the `lobbyArea` handle area, making it possible to listen for clicks.
+func (ui *SettingsUI) DrawLobbyGames(lobbyGames []client.LobbyGame) {
+	ui.clearLobby()
+
+	lCfg := wGui.NewButtonConfig()
+	lCfg.Width = 21
+	ui.lobbyMap = make(map[string]Row, 0)
+	areaMap := make(map[string]wGui.Physical)
+	for i, game := range lobbyGames {
+		y := 18 + (i+1)*3
+		btns := []*wGui.Button{
+			wGui.NewButton(2, y, game.Nick, lCfg),
+			wGui.NewButton(lCfg.Width+2, y, game.Status, lCfg),
+		}
+		ui.Controller.Draw(btns[0])
+		ui.Controller.Draw(btns[1])
+		row := NewRow(btns)
+		areaMap[game.Nick] = row
+		ui.lobbyMap[game.Nick] = row
+	}
+	ui.lobbyArea.SetClickablesOn(areaMap)
+	ui.Controller.Draw(ui.lobbyArea)
+}
+
+// Removes all the lobby rows and their handle area from the screen.
+func (ui *SettingsUI) clearLobby() {
+	ui.Controller.Remove(ui.lobbyArea)
+	for _, row := range ui.lobbyMap {
+		btns := row.GetButtons()
+		ui.Controller.Remove(btns[0])
+		ui.Controller.Remove(btns[1])
+	}
+}
+
+// Listens for a click in the lobby, and then toggles the row that was clicked.
+func (ui *SettingsUI) ListenLobby(ctx context.Context) {
+	clickedNick := ui.lobbyArea.Listen(ctx)
+	ui.ToggleOpponent(clickedNick)
+}
+
+// It is responsible for the possibility to select an opponent and changes the graphical interface depending on the given nickname of the opponent.
+func (ui *SettingsUI) ToggleOpponent(nick string) {
+	// If nick is not present in the lobby - end.
+	row, ok := ui.lobbyMap[nick]
+	if !ok {
+		return
+	}
+	// If the nickname is the same as the already selected opponent, deselect it.
+	if ui.targetNick == nick {
+		row.SetBgColor(wGui.Black)
+		row.SetFgColor(wGui.White)
+		ui.targetNick = ""
+		ui.startBtn.SetBgColor(wGui.Green)
+		ui.startBtn.SetText("Host Game")
+		ui.targetRow = nil
+		return
+	}
+	// else - select given opponent.
+	row.SetBgColor(wGui.White)
+	row.SetFgColor(wGui.Black)
+	ui.targetNick = nick
+	ui.startBtn.SetBgColor(wGui.Red)
+	ui.startBtn.SetText("Fight Opponent")
+	// If another opponent was selected before, change the colour of their row to normal.
+	if ui.targetRow != nil {
+		ui.targetRow.SetBgColor(wGui.Black)
+		ui.targetRow.SetFgColor(wGui.White)
+	}
+	ui.targetRow = &row
+}
+
+func (ui *SettingsUI) TargetNick() string {
+	return ui.targetNick
+}
+
+func (ui *SettingsUI) Nick() string {
+	return ui.nameInput.GetText()
+}
+
+func (ui *SettingsUI) Desc() string {
+	return ui.descInput.GetText()
+}
--- logic/settings.go
+++ logic/settings.go
@@ -4,172 +4,67 @@ import (
 	"battleship_client/api/client"
 	"battleship_client/gui/cli"
 	"context"
+	"fmt"
 
 	wGui "github.com/RostKoff/warships-gui/v2"
 )
 
-func DisplayGameSettings(gui *wGui.GUI, ch chan<- client.GameSettings) {
+// Displays game settings and listens for button clicks.
+// When the start button or bot button is clicked it sends game settings to the channel given as the argument.
+func DisplayGameSettings(controller *wGui.GUI, ch chan<- client.GameSettings) {
 	refresh := make(chan rune)
 
-	gui.NewScreen("settings")
-	gui.SetScreen("settings")
+	controller.NewScreen("settings")
+	controller.SetScreen("settings")
 
-	// Name Input
-	nameTxt := wGui.NewText(2, 1, "Enter name", nil)
-	nameInCfg := wGui.NewTextFieldConfig()
-	nameInCfg.UnfilledChar = '_'
-	nameInCfg.InputOn = true
-	nameIn := wGui.NewTextField(2, 2, 30, 1, nameInCfg)
+	settingsUi := cli.InitSettings(controller)
 
-	// Description Input
-	descTxt := wGui.NewText(2, 4, "Enter Description", nil)
-	descInCfg := wGui.NewTextFieldConfig()
-	descInCfg.UnfilledChar = '.'
-	descInCfg.InputOn = true
-	descIn := wGui.NewTextField(2, 5, 30, 5, descInCfg)
-
-	// Action Buttons
-	btnCfg := wGui.NewButtonConfig()
-	btnCfg.Width = 0
-	btnCfg.Height = 0
-	btnCfg.Width = 20
-	btnCfg.BgColor = wGui.Green
-	startBtn := wGui.NewButton(2, 11, "Host Game", btnCfg)
-	x, _ := startBtn.Position()
-	w, _ := startBtn.Size()
-	btnCfg.BgColor = wGui.Blue
-	botBtn := wGui.NewButton(x+w+2, 11, "Against Bot", btnCfg)
-	x, _ = botBtn.Position()
-	w, _ = botBtn.Size()
-	btnCfg.BgColor = wGui.Grey
-	refreshBtn := wGui.NewButton(x+w+2, 11, "Refresh", btnCfg)
-
-	// Handle Area for buttons
-	btnMapping := map[string]wGui.Physical{
-		"botBtn":     botBtn,
-		"startBtn":   startBtn,
-		"refreshBtn": refreshBtn,
-	}
-	btnArea := wGui.NewHandleArea(btnMapping)
-
-	// Lobby
-	lCfg := wGui.NewButtonConfig()
-	lCfg.Width = 42
-
-	lobbyTxt := wGui.NewButton(2, 15, "Lobby", lCfg)
-	lCfg.Width = 21
-	statusHead := wGui.NewButton(2, 18, "Status", lCfg)
-	nickHead := wGui.NewButton(lCfg.Width+2, 18, "Nick", lCfg)
-
-	drawables := []wGui.Drawable{
-		nameTxt, nameIn, descTxt, descIn, btnArea, botBtn, startBtn, lobbyTxt, statusHead, nickHead, refreshBtn,
-	}
-
-	areaMap := make(map[string]wGui.Physical)
-	lobbyArea := wGui.NewHandleArea(areaMap)
-	lobbyMap := make(map[string]cli.Row)
-	go func() {
-		for {
-			lobbyMap = getLobbyGames()
-			for key, value := range lobbyMap {
-				areaMap[key] = value
-				btns := value.GetButtons()
-				gui.Draw(btns[0])
-				gui.Draw(btns[1])
-			}
-			lobbyArea.SetClickablesOn(areaMap)
-			gui.Draw(lobbyArea)
-			<-refresh
-			gui.Remove(lobbyArea)
-			for key, value := range lobbyMap {
-				btns := value.GetButtons()
-				gui.Remove(btns[0])
-				gui.Remove(btns[1])
-				delete(areaMap, key)
-			}
-		}
-	}()
-
-	// Draw objects
-	for _, drawable := range drawables {
-		gui.Draw(drawable)
-	}
+	go displayLobby(settingsUi, refresh)
 
 	ctx := context.Background()
-	targetNick := ""
-	targetRow := new(cli.Row)
-	go func() {
-		for {
-			clickedNick := lobbyArea.Listen(ctx)
-			row, ok := lobbyMap[clickedNick]
-			if !ok {
-				continue
-			}
-			if targetNick == clickedNick {
-				row.SetBgColor(wGui.Black)
-				row.SetFgColor(wGui.White)
-				targetNick = ""
-				startBtn.SetBgColor(wGui.Green)
-				startBtn.SetText("Host Game")
-				targetRow = nil
-				continue
-			}
-			row.SetBgColor(wGui.White)
-			row.SetFgColor(wGui.Black)
-			targetNick = clickedNick
-			startBtn.SetBgColor(wGui.Red)
-			startBtn.SetText("Fight Opponent")
-			if targetRow != nil {
-				targetRow.SetBgColor(wGui.Black)
-				targetRow.SetFgColor(wGui.White)
-			}
-			targetRow = &row
-		}
-	}()
+	go handleLobby(settingsUi, ctx)
 
+	// Handle button clicks.
 	for {
-		clicked := btnArea.Listen(ctx)
+		clicked := settingsUi.BtnArea.Listen(ctx)
 		switch clicked {
 		case "botBtn":
 			ch <- client.GameSettings{
 				AgainstBot:  true,
-				Nick:        nameIn.GetText(),
-				Description: descIn.GetText(),
+				Nick:        settingsUi.Nick(),
+				Description: settingsUi.Desc(),
 			}
+			return
 		case "startBtn":
 			ch <- client.GameSettings{
 				AgainstBot:  false,
-				Nick:        nameIn.GetText(),
-				Description: descIn.GetText(),
-				TargetNick:  targetNick,
+				Nick:        settingsUi.Nick(),
+				Description: settingsUi.Desc(),
+				TargetNick:  settingsUi.TargetNick(),
 			}
+			return
 		case "refreshBtn":
-			targetNick = ""
-			startBtn.SetBgColor(wGui.Green)
-			startBtn.SetText("Host Game")
-			targetRow = nil
+			settingsUi.ToggleOpponent(settingsUi.TargetNick())
 			refresh <- 'r'
 		}
 	}
 }
 
-func getLobbyGames() map[string]cli.Row {
-	lobbyGames, err := client.Lobby()
-	lCfg := wGui.NewButtonConfig()
-	lCfg.Width = 21
-	if err != nil {
-		return nil
-	}
-	lobbyMap := make(map[string]cli.Row, 0)
-	for i, game := range lobbyGames {
-		y := 18 + (i+1)*3
-		btns := []*wGui.Button{
-			wGui.NewButton(2, y, game.Status, lCfg),
-			wGui.NewButton(lCfg.Width+2, y, game.Nick, lCfg),
+// Fetches game lobbies from the API, displays them on the screen and waits until any rune is sent to the channel given as the argument.
+func displayLobby(ui *cli.SettingsUI, refresh <-chan rune) {
+	for {
+		lobbyGames, err := client.Lobby()
+		if err != nil {
+			ui.Controller.Log(fmt.Sprintf("failed to get game lobby: %s", err.Error()))
+			lobbyGames = nil
 		}
-
-		row := cli.NewRow(btns)
-		lobbyMap[game.Nick] = row
+		ui.DrawLobbyGames(lobbyGames)
+		<-refresh
 	}
-	return lobbyMap
+}
+
+func handleLobby(ui *cli.SettingsUI, ctx context.Context) {
+	for {
+		ui.ListenLobby(ctx)
+	}
 }
